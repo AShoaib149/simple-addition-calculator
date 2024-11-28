26d5fc39a95074d51254f847ce79fbceb2603f29
AShoaib149 <a.abrahimkhil@share.epsb.ca>
2024-11-28 14:15:59 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// SIMPLE ADDITION IPO CALCULATOR
+
+// Input
+let number1 = +prompt("Enter a number:");
+let number2 = +prompt("Enter another number:");
+
+// Process
+let total = number1 + number2;
+
+// Output
+alert(`${number1} + ${number2} = ${total}`);
